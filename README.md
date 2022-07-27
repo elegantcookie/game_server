f696e06c327e7c2372e57a250f0f038334a8e594
Michael <gladiolud64@gmail.com>
2022-07-27 14:00:56 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+<h1>game server REST API</h1>
+
+<h3>API Gateway</h3>
+
+<div>
+<h4>/heartbeat</h4>
+<p>
+returns 204 if the service is up 
+</p>
+</div>
+
+
+<h3>Auth Service</h3>
+<div>
+<h4>/heartbeat</h4>
+<p>
+returns 204 if the service is up 
+</p>
+</div>
+
+
+<h3>User Service</h3>
+<div>
+<h4>/heartbeat</h4>
+<p>
+returns 204 if the service is up 
+</p>
+</div>
---
+++ user_service/cmd/app/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"user_service/internal/app"
+	"user_service/internal/config"
+	"user_service/pkg/logging"
+)
+
+func main() {
+	log.Print("config initialization")
+	cfg := config.GetConfig()
+
+	log.Printf("logging initialized.")
+
+	logger := logging.GetLogger(cfg.AppConfig.LogLevel)
+
+	a, err := app.NewApp(cfg, &logger)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	logger.Println("running Application")
+	a.Run()
+}
