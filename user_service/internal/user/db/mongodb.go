2a145619e8f5d751fbc06bf3782f3d5dd1153722
ecookie <gladiolud64@gmail.com>
2022-07-27 16:51:05 +0700

    added delete method

--- user_service/internal/user/db/mongodb.go
+++ user_service/internal/user/db/mongodb.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -37,7 +38,9 @@ func (d *db) FindOne(ctx context.Context, id string) (u user.User, err error) {
 	filter := bson.M{"_id": oid}
 	result := d.collection.FindOne(ctx, filter)
 	if result.Err() != nil {
-		// TODO 404
+		if errors.Is(result.Err(), mongo.ErrNoDocuments) {
+			// TODO ErrEntityNotFound
+		}
 		return u, fmt.Errorf("failed to find one user by id: %s due to error: %v", id, result.Err())
 	}
 	if err = result.Decode(&u); err != nil {
@@ -81,7 +84,24 @@ func (d *db) Update(ctx context.Context, user user.User) error {
 	return nil
 }
 func (d *db) Delete(ctx context.Context, id string) error {
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return fmt.Errorf("failed to convert user ID to ObjectID. ID=%v", id)
+	}
 
+	filter := bson.M{"_id": objectID}
+
+	result, err := d.collection.DeleteOne(ctx, filter)
+	if err != nil {
+		return fmt.Errorf("failed to execute update user query due to: %v", err)
+	}
+
+	if result.DeletedCount == 0 {
+		// TODO ErrEntityNotFound
+		return fmt.Errorf("not found")
+	}
+	d.logger.Tracef("Deleted %d documents", result.DeletedCount)
+	return nil
 }
 
 func NewStorage(database *mongo.Database, collection string, logger *logging.Logger) user.Storage {
