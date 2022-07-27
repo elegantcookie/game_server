3c74ed7134410e9158c6178a060b94d63fb29d65
ecookie <gladiolud64@gmail.com>
2022-07-27 15:46:40 +0700

    added find one method

--- user_service/internal/user/db/mongodb.go
+++ user_service/internal/user/db/mongodb.go
@@ -3,6 +3,7 @@ package db
 import (
 	"context"
 	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"user_service/internal/user"
@@ -26,9 +27,23 @@ func (d *db) Create(ctx context.Context, user user.User) (string, error) {
 		return oid.Hex(), nil
 	}
 	d.logger.Trace(user)
-	return "", fmt.Errorf("failed to convert objectId to hex due to: %v", err)
+	return "", fmt.Errorf("failed to convert objectId to hex. probable oid: %s", oid)
 }
-func (d *db) FindOne(ctx context.Context, id string) (user.User, error) {
+func (d *db) FindOne(ctx context.Context, id string) (u user.User, err error) {
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return u, fmt.Errorf("failed to convert hex to objectID, hex: %s", id)
+	}
+	filter := bson.M{"_id": oid}
+	result := d.collection.FindOne(ctx, filter)
+	if result.Err() != nil {
+		// TODO 404
+		return u, fmt.Errorf("failed to find one user by id: %s due to error: %v", id, result.Err())
+	}
+	if err = result.Decode(&u); err != nil {
+		return u, fmt.Errorf("failed to decode user(id:%s) from DB due to error: %v", id, err)
+	}
+	return u, nil
 
 }
 func (d *db) Update(ctx context.Context, user user.User) error {
