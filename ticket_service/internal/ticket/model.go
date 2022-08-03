ce90730f18f5201dd542ec93925d13cbb5784c63
ecookie <gladiolud64@gmail.com>
2022-08-03 12:30:12 +0700

    added ticket status endpoint

--- ticket_service/internal/ticket/model.go
+++ ticket_service/internal/ticket/model.go
@@ -2,7 +2,7 @@ package ticket
 
 type Ticket struct {
 	ID           string `json:"id" bson:"_id,omitempty"`
-	Type         string `json:"type" bson:"type"`
+	IsActive     bool   `json:"is_active" bson:"is_active"`
 	TicketPrice  int    `json:"ticket_price" bson:"ticket_price"`
 	PlayerAmount int    `json:"player_amount" bson:"player_amount"`
 	GameType     string `json:"game_type" bson:"game_type"`
@@ -10,7 +10,6 @@ type Ticket struct {
 }
 
 type TicketDTO struct {
-	Type         string `json:"type"`
 	TicketPrice  int    `json:"ticket_price"`
 	PlayerAmount int    `json:"player_amount"`
 	GameType     string `json:"game_type"`
