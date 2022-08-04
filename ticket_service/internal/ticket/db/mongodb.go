ada2c95f7ec9be848e6957ffcd62b803d941fecc
ecookie <gladiolud64@gmail.com>
2022-08-04 22:58:33 +0700

    docs updated

--- ticket_service/internal/ticket/db/mongodb.go
+++ ticket_service/internal/ticket/db/mongodb.go
@@ -19,7 +19,7 @@ type db struct {
 func (d *db) Create(ctx context.Context, ticket ticket.Ticket) (string, error) {
 	result, err := d.collection.InsertOne(ctx, ticket)
 	if err != nil {
-		return "", fmt.Errorf("failed to create ticket due to: %v", err)
+		return "", fmt.Errorf("failed to create prize due to: %v", err)
 	}
 	d.logger.Debug("convert InsertedID to objectID")
 	oid, ok := result.InsertedID.(primitive.ObjectID)
@@ -30,7 +30,7 @@ func (d *db) Create(ctx context.Context, ticket ticket.Ticket) (string, error) {
 	return "", fmt.Errorf("failed to convert objectId to hex. probable oid: %s", oid)
 }
 
-// FindById find ticket by ticketID
+// FindById find prize by ticketID
 func (d *db) FindById(ctx context.Context, id string) (ticket ticket.Ticket, err error) {
 	oid, err := primitive.ObjectIDFromHex(id)
 	if err != nil {
@@ -42,10 +42,10 @@ func (d *db) FindById(ctx context.Context, id string) (ticket ticket.Ticket, err
 		if errors.Is(result.Err(), mongo.ErrNoDocuments) {
 			// TODO ErrEntityNotFound
 		}
-		return ticket, fmt.Errorf("failed to find ticket by id: %s due to error: %v", id, result.Err())
+		return ticket, fmt.Errorf("failed to find prize by id: %s due to error: %v", id, result.Err())
 	}
 	if err = result.Decode(&ticket); err != nil {
-		return ticket, fmt.Errorf("failed to decode ticket(id:%s) from DB due to error: %v", id, err)
+		return ticket, fmt.Errorf("failed to decode prize(id:%s) from DB due to error: %v", id, err)
 	}
 	return ticket, nil
 }
@@ -65,20 +65,20 @@ func (d *db) FindAll(ctx context.Context) (users []ticket.Ticket, err error) {
 func (d *db) Update(ctx context.Context, ticket ticket.Ticket) error {
 	objectID, err := primitive.ObjectIDFromHex(ticket.ID)
 	if err != nil {
-		return fmt.Errorf("failed to convert ticket ID to ObjectID. ID=%v", ticket.ID)
+		return fmt.Errorf("failed to convert prize ID to ObjectID. ID=%v", ticket.ID)
 	}
 
 	filter := bson.M{"_id": objectID}
 
 	userBytes, err := bson.Marshal(ticket)
 	if err != nil {
-		return fmt.Errorf("failed to marshal ticket due to: %v", err)
+		return fmt.Errorf("failed to marshal prize due to: %v", err)
 	}
 
 	var updateUserObj bson.M
 	err = bson.Unmarshal(userBytes, &updateUserObj)
 	if err != nil {
-		return fmt.Errorf("failed to unmarshal ticket bytes due to: %v", err)
+		return fmt.Errorf("failed to unmarshal prize bytes due to: %v", err)
 	}
 	delete(updateUserObj, "_id")
 	update := bson.M{
@@ -86,7 +86,7 @@ func (d *db) Update(ctx context.Context, ticket ticket.Ticket) error {
 	}
 	result, err := d.collection.UpdateOne(ctx, filter, update)
 	if err != nil {
-		return fmt.Errorf("failed to execute update ticket query due to: %v", err)
+		return fmt.Errorf("failed to execute update prize query due to: %v", err)
 	}
 
 	if result.MatchedCount == 0 {
@@ -96,17 +96,17 @@ func (d *db) Update(ctx context.Context, ticket ticket.Ticket) error {
 	return nil
 }
 
-// Delete ticket by ticketID
+// Delete prize by ticketID
 func (d *db) Delete(ctx context.Context, id string) error {
 	objectID, err := primitive.ObjectIDFromHex(id)
 	if err != nil {
-		return fmt.Errorf("failed to convert ticket ID to ObjectID. ID=%v", id)
+		return fmt.Errorf("failed to convert prize ID to ObjectID. ID=%v", id)
 	}
 
 	filter := bson.M{"_id": objectID}
 	result, err := d.collection.DeleteOne(ctx, filter)
 	if err != nil {
-		return fmt.Errorf("failed to execute update ticket query due to: %v", err)
+		return fmt.Errorf("failed to execute update prize query due to: %v", err)
 	}
 
 	if result.DeletedCount == 0 {
